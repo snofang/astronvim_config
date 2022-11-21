c6feaf9fa5ac1d035b367c2f1df1f1f1880ed1bf
Mehdi Taghizadeh <snofang@gmail.com>
2022-11-21 22:20:29 +0330

    disabling auto format on save & time consuming Latext and markdown

--- init.lua
+++ init.lua
@@ -6,24 +6,24 @@ local config = {
     -- installing plugins
     init = {
       ["windwp/nvim-autopairs"] = { disable = true },
-      {"folke/tokyonight.nvim"}, -- theme
+      { "folke/tokyonight.nvim" }, -- theme
       {
         "windwp/nvim-spectre",
-        requires = {"nvim-lua/plenary.nvim"}, -- search & replace
+        requires = { "nvim-lua/plenary.nvim" }, -- search & replace
         -- config = function ()
         --   local map = vim.api.nvim_set_keymap
         --   map("n", "<leader>fr", "<cmd>lua require('spectre').open()<CR>", { desc = "open find & replace" })
         -- end,
       },
-      {"mhinz/vim-mix-format"},
+      { "mhinz/vim-mix-format" },
     },
 
     -- overriding setup() of existing plugins
-    cmp = function (config)
+    cmp = function(config)
       local cmp_ok, cmp = pcall(require, "cmp")
       local luasnip_ok, luasnip = pcall(require, "luasnip")
       if cmp_ok and luasnip_ok then
-        config.mapping["<CR>"] = cmp.mapping.confirm ({ select = false })
+        config.mapping["<CR>"] = cmp.mapping.confirm({ select = false })
         config.mapping["<C-y>"] = cmp.mapping.confirm { select = true, behavior = cmp.ConfirmBehavior.Insert }
         config.mapping["<Tab>"] = cmp.mapping(function(fallback)
           if luasnip.expandable() then
@@ -67,9 +67,9 @@ local config = {
         "http",
         "javascript",
         "json",
-        "latex",
+        -- "latex",
         "lua",
-        "markdown",
+        -- "markdown",
         "regex",
         "vim",
         "yaml",
@@ -85,7 +85,7 @@ local config = {
         "sumneko_lua",
         -- "elixirls",
         "jsonls",
-        "ltex", --Latex/Markdown
+        -- "ltex", --Latex/Markdown
         "yamlls",
         "sqlls",
         "erlangls",
@@ -96,59 +96,62 @@ local config = {
     },
 
 
-    ["null-ls"] = function(config)
-      local null_ls = require "null-ls"
-      config.sources = {
-        -- null_ls.builtins.diagnostics.credo,
-        null_ls.builtins.formatting.mix,
-        null_ls.builtins.formatting.surface,
-      }
-      -- set up null-ls's on_attach function
-      -- config.on_attach = function(client)
-      --   -- NOTE: You can remove this on attach function to disable format on save
-      --   if client.resolved_capabilities.document_formatting then
-      --     vim.api.nvim_create_autocmd("BufWritePre", {
-      --       desc = "Auto format before save",
-      --       pattern = "<buffer>",
-      --       callback = vim.lsp.buf.formatting_sync,
-      --     })
-      --   end
-      -- end
-      return config -- return final config table
-    end,
+    -- ["null-ls"] = function(config)
+    --   local null_ls = require "null-ls"
+    --   config.sources = {
+    --     -- null_ls.builtins.diagnostics.credo,
+    --     null_ls.builtins.formatting.mix,
+    --     null_ls.builtins.formatting.surface,
+    --   }
+    --   -- set up null-ls's on_attach function
+    --   -- config.on_attach = function(client)
+    --   --   -- NOTE: You can remove this on attach function to disable format on save
+    --   --   if client.resolved_capabilities.document_formatting then
+    --   --     vim.api.nvim_create_autocmd("BufWritePre", {
+    --   --       desc = "Auto format before save",
+    --   --       pattern = "<buffer>",
+    --   --       callback = vim.lsp.buf.formatting_sync,
+    --   --     })
+    --   --   end
+    --   -- end
+    --   return config -- return final config table
+    -- end,
 
   },
 
   -- to change the default configuration of the elixirls; all of these failed to work :~
   lsp = {
+    formatting = {
+      format_on_save = false,
+    },
     servers = {
       "elixirls",
     },
     ["server-settings"] = {
       elixirls = {
-          cmd = {path_to_elixirls_server},
-          root_dir = function()
-              return os.getenv("PWD")--util.root_pattern(".git")(fname)
-          end,
-          suggestSpecs = false,
-          dialyzerEnabled = true,
-          signatureAfterComplete = false,
-          fetchDeps = false,
-          capabilities = {
-            document_formatting = true
-          }
-          -- on_attach = function(client)
-          --   client.server_capabilities.document_formatting = true
-          --   on_attach(client)
-          -- end,
+        cmd = { path_to_elixirls_server },
+        root_dir = function()
+          return os.getenv("PWD") --util.root_pattern(".git")(fname)
+        end,
+        suggestSpecs = false,
+        dialyzerEnabled = true,
+        signatureAfterComplete = false,
+        fetchDeps = false,
+        capabilities = {
+          document_formatting = true
+        }
+        -- on_attach = function(client)
+        --   client.server_capabilities.document_formatting = true
+        --   on_attach(client)
+        -- end,
       },
       html = {
-        filetypes =  {"heex", "html", "eelixir"}
+        filetypes = { "heex", "html", "eelixir" }
       }
     }
   },
 
-  polish = function ()
+  polish = function()
     -- theme
     vim.g.tokyonight_style = "storm" --"night"
     vim.api.nvim_command(("colorscheme %s"):format("tokyonight"))
