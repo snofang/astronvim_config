c4fda03bea7707d0f3e0292b24be33c0be17ddd9
Mehdi Taghizadeh <snofang@gmail.com>
2023-04-17 19:19:59 +0330

    making tokyonight style dark

--- plugins/tokyonight.lua
+++ plugins/tokyonight.lua
@@ -3,8 +3,8 @@ return {
   -- lazy = false,
   -- priority = 1000,
   opts = {
-    style = "moon",                     -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-    transparent = vim.g.neovide == nil, -- Enable this to disable setting the background color
+    style = "night",                     -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+    -- transparent = vim.g.neovide == nil, -- Enable this to disable setting the background color
     -- terminal_colors = true,             -- Configure the colors used when opening a `:terminal` in Neovim                                                                                                  |
     -- styles = {
     --   -- Style to be applied to different syntax groups
